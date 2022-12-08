decbc51a494bde0a455cf467455625d3785fbc21
jinsubaek <bjinsu93@gmail.com>
2022-12-09 00:29:10 +0900

    feat: 선형 변환 행렬 추가

--- src/math/matrix.ts
+++ src/math/matrix.ts
@@ -1,22 +1,6 @@
+import { degreeToRadian } from "./util";
 
 const MATRIX4X4_LENGTH = 16;
-
-export class Matrix {
-    public static getProjectionMatrix(
-        fov: number, aspect: number, near: number, far: number
-    ): Matrix4x4 {
-
-        const matrix: Matrix4x4 = new Matrix4x4()
-        matrix.data[0] = aspect * (1 / Math.tan(fov / 2));
-        matrix.data[5] = 1 / Math.tan(fov / 2);
-        matrix.data[10] = far / (far - near);
-        matrix.data[11] = (-far * near) / (far - near);
-        matrix.data[14] = 1.0;
-
-        return matrix;
-    }
-
-}
 
 export class Matrix4x4 {
     private _data: number[] = [];
@@ -38,3 +22,99 @@ export class Matrix4x4 {
         return this._data;
     }
 }
+
+export class Matrix {
+    public static identity(): Matrix4x4 {
+        return new Matrix4x4(
+            [
+                1, 0, 0, 0, 
+                0, 1, 0, 0,
+                0, 0, 1, 0, 
+                0, 0, 0, 1
+            ]
+        );
+    }
+
+    public static scale4x4(x: number, y: number, z: number): Matrix4x4 {
+        const matrix: Matrix4x4 = new Matrix4x4();
+
+        matrix.data[0] = x;
+        matrix.data[5] = y;
+        matrix.data[10] = z;
+        matrix.data[15] = 1;
+
+        return matrix;
+    }
+
+    public static translation4x4(x: number, y: number, z: number): Matrix4x4 {
+        const matrix: Matrix4x4 = this.identity();
+        
+        matrix.data[3] = x;
+        matrix.data[7] = y;
+        matrix.data[11] = z;
+        matrix.data[15] = 1;
+
+        return matrix;
+    }
+
+    public static rotationX4x4(deg: number): Matrix4x4 {
+        const matrix: Matrix4x4 = this.identity();
+        
+        const radian = degreeToRadian(deg);
+        const cos = Math.cos(radian);
+        const sin = Math.sin(radian);
+
+        matrix.data[5] = cos;
+        matrix.data[6] = -sin;
+        matrix.data[9] = sin;
+        matrix.data[10] = cos;
+
+        return matrix;
+    }
+
+    public static rotationY4x4(deg: number): Matrix4x4 {
+        const matrix: Matrix4x4 = this.identity();
+    
+        const radian = degreeToRadian(deg);
+        const cos = Math.cos(radian);
+        const sin = Math.sin(radian);
+
+        matrix.data[0] = cos;
+        matrix.data[3] = sin;
+        matrix.data[8] = -sin;
+        matrix.data[10] = cos;
+
+        return matrix;
+    }
+
+    public static rotationZ4x4(deg: number): Matrix4x4 {
+        const matrix: Matrix4x4 = this.identity();
+        
+        const radian = degreeToRadian(deg);
+        const cos = Math.cos(radian);
+        const sin = Math.sin(radian);
+
+        matrix.data[0] = cos;
+        matrix.data[1] = -sin;
+        matrix.data[4] = sin;
+        matrix.data[5] = cos;
+
+        return matrix;
+    }
+
+    public static projection(
+        fov: number, aspect: number, near: number, far: number
+    ): Matrix4x4 {
+
+        const matrix: Matrix4x4 = new Matrix4x4()
+        matrix.data[0] = aspect * (1 / Math.tan(fov / 2));
+        matrix.data[5] = 1 / Math.tan(fov / 2);
+        matrix.data[10] = far / (far - near);
+        matrix.data[11] = (-far * near) / (far - near);
+        matrix.data[14] = 1.0;
+
+        return matrix;
+    }
+
+}
+
