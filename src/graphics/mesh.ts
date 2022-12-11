4b7cc4d0d5718896625567a65f4dfe01e6c9290e
jinsubaek <bjinsu93@gmail.com>
2022-12-11 22:24:08 +0900

    feat: obj 파일 파싱하는 기능 추가

---
+++ src/graphics/mesh.ts
@@ -0,0 +1,88 @@
+import { Vector, Vector3 } from "../math/vector"
+import { Face } from "./triangle";
+
+import ObjFileParser from "obj-file-parser"
+
+export interface Mesh {
+    vertices: Vector3[];
+    faces:  Face[];
+    rotation: Vector3;
+};
+
+export const MESH_VERTICES: Vector3[] = [
+    { x: -1, y: -1, z: -1 } as Vector3, // 1
+    { x: -1, y:  1, z: -1 } as Vector3, // 2
+    { x:  1, y:  1, z: -1 } as Vector3, // 3 
+    { x:  1, y: -1, z: -1 } as Vector3, // 4
+    { x:  1, y:  1, z:  1 } as Vector3, // 5 
+    { x:  1, y: -1, z:  1 } as Vector3, // 6
+    { x: -1, y:  1, z:  1 } as Vector3, // 7 
+    { x: -1, y: -1, z:  1 } as Vector3  // 8
+];
+
+export const MESH_FACES: Face[] = [
+    // front
+    { a: 1, b: 2, c: 3 },
+    { a: 1, b: 3, c: 4 },
+
+    // right
+    { a: 4, b: 3, c: 5 },
+    { a: 4, b: 5, c: 6 },
+
+    // back
+    { a: 6, b: 5, c: 7 },
+    { a: 6, b: 7, c: 8 },
+
+    // left
+    { a: 8, b: 7, c: 2 },
+    { a: 8, b: 2, c: 1 },
+
+    // top
+    { a: 2, b: 7, c: 5 },
+    { a: 2, b: 5, c: 3 },
+
+    // bottom
+    { a: 6, b: 8, c: 1 },
+    { a: 6, b: 1, c: 4 },
+];
+
+export const loadCubeMeshData = (): Mesh => {
+    let cubeMesh: Mesh = {
+        vertices: [], faces: [], rotation: { x: 0, y: 0, z: 0 } as Vector3
+    };
+
+    for (let i = 0; i < MESH_VERTICES.length; i++) {
+        cubeMesh.vertices.push(MESH_VERTICES[i]);
+    }
+
+    for (let i = 0; i < MESH_FACES.length; i++) {
+        cubeMesh.faces.push(MESH_FACES[i]);
+    }
+
+    return cubeMesh;
+}
+
+export const loadObjFileData = async (filePath: string): Promise<Mesh> => {
+    const mesh: Mesh = {vertices: [], faces: [], rotation: { x: 0, y: 0, z: 0 } as Vector3};
+
+    const res = await fetch(filePath);
+    const text = await res.text();
+    const objFile = new ObjFileParser(text);
+    const output = objFile.parse();
+
+    mesh.vertices = output.models[0].vertices.map((v) => new Vector3(v.x, v.y, v.z));
+
+    mesh.faces = output.models[0].faces.map((face) => ({ 
+            a: face.vertices[0].vertexIndex, 
+            b: face.vertices[1].vertexIndex, 
+            c: face.vertices[2].vertexIndex 
+        })
+    );
+    //mesh.faces = face.fla
+    // mesh.faces = output.models[0].faces.map((face) => 
+    //     face.vertices.map((v) => ({a: v.vertexIndex, b: v.textureCoordsIndex, c: v.vertexNormalIndex}) )
+    // )
+    //console.log(face);
+   
+    return mesh;
+}
