485a928702b0227cdd18a66edeeda1477d6d3593
jinsubaek <bjinsu93@gmail.com>
2022-12-13 00:15:15 +0900

    feat: 벡터 연산에 필요한 함수들 추가

--- src/math/vector.ts
+++ src/math/vector.ts
@@ -5,7 +5,7 @@ export class Vector {
         return new Vector4(a.x, a.y, a.z, 1.0);
     }
 
-    public static multiplyMatrix(m: Matrix4x4, v: Vector4) {
+    public static multiplyMatrix4x4(m: Matrix4x4, v: Vector4) {
         const x = m.data[0] * v.x + m.data[1] * v.y + m.data[2] * v.z + m.data[3] * v.w;
         const y = m.data[4] * v.x + m.data[5] * v.y + m.data[6] * v.z + m.data[7] * v.w;
         const z = m.data[8] * v.x + m.data[9] * v.y + m.data[10] * v.z + m.data[11] * v.w;
@@ -16,6 +16,7 @@ export class Vector {
 
     public static rotateXvec3(v: Vector3, r: number): Vector3 {
         const vector = new Vector3();
+
         vector.x = v.x;          
         vector.y = Math.cos(r) * v.y - Math.sin(r) * v.z;
         vector.z = Math.sin(r) * v.y + Math.cos(r) * v.z;
@@ -25,6 +26,7 @@ export class Vector {
 
     public static rotateYvec3(v: Vector3, r: number): Vector3 {
         const vector = new Vector3();
+
         vector.x = Math.cos(r) * v.x + Math.sin(r) * v.z;
         vector.y = v.y;          
         vector.z = -Math.sin(r) * v.x + Math.cos(r) * v.z;
@@ -34,11 +36,61 @@ export class Vector {
 
     public static rotateZvec3(v: Vector3, r: number): Vector3 {
         const vector = new Vector3();
+
         vector.x = Math.cos(r) * v.x - Math.sin(r) * v.y;
         vector.y = Math.sin(r) * v.x + Math.cos(r) * v.y;
         vector.z = v.z;          
         
         return vector;
+    }
+
+    public static subtractVec3(v1: Vector3, v2: Vector3): Vector3 {
+        const vector = new Vector3();
+
+        vector.x = v1.x - v2.x;
+        vector.y = v1.y - v2.y;
+        vector.z = v1.z - v2.z;
+
+        return vector;
+    }
+
+    public static addVec3(v1: Vector3, v2: Vector3): Vector3 {
+        const vector = new Vector3();
+
+        vector.x = v1.x + v2.x;
+        vector.y = v1.y + v2.y;
+        vector.z = v1.z + v2.z;
+
+        return vector;
+    }
+
+    public static dotVec3(v1: Vector3, v2: Vector3): number {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    }
+
+    public static crossVec3(v1: Vector3, v2: Vector3): Vector3 {
+        const vector: Vector3 = new Vector3();
+
+        vector.x = v1.y * v2.z - v1.z * v2.y;
+        vector.y = v1.z * v2.x - v1.x * v2.z;
+        vector.z = v1.x * v2.y - v1.y * v2.x;
+
+        return vector;
+    }
+
+    public static normalizeVec3(v: Vector3): Vector3 {
+        const vector: Vector3 = new Vector3();
+        const length: number = this.magnitudeVec3(v);
+
+        vector.x = v.x / length;
+        vector.y = v.y / length;
+        vector.z = v.z / length;
+
+        return vector;
+    }
+
+    public static magnitudeVec3(v: Vector3): number {
+        return Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
     }
 }
 
