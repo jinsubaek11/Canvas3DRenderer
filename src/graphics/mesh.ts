38ca47e0f06b301b15a338ef539885a33d77b2ba
jinsubaek <bjinsu93@gmail.com>
2022-12-22 02:18:09 +0900

    feat: obj파일에서 텍스쳐 좌표 추가

--- src/graphics/mesh.ts
+++ src/graphics/mesh.ts
@@ -23,28 +23,28 @@ export const MESH_VERTICES: Vector3[] = [
 
 export const MESH_FACES: Face[] = [
     // front
-    { a: 1, b: 2, c: 3, uvA: { u: 0, v: 0 }, uvB: { u: 0, v: 1 }, uvC: { u: 1, v: 1 } },
-    { a: 1, b: 3, c: 4, uvA: { u: 0, v: 0 }, uvB: { u: 1, v: 1 }, uvC: { u: 1, v: 0 } },
+    { a: 1, b: 2, c: 3, uvA: { u: 0, v: 1 }, uvB: { u: 0, v: 0 }, uvC: { u: 1, v: 0 } },
+    { a: 1, b: 3, c: 4, uvA: { u: 0, v: 1 }, uvB: { u: 1, v: 0 }, uvC: { u: 1, v: 1 } },
 
     // right
-    { a: 4, b: 3, c: 5, uvA: { u: 0, v: 0 }, uvB: { u: 0, v: 1 }, uvC: { u: 1, v: 1 } },
-    { a: 4, b: 5, c: 6, uvA: { u: 0, v: 0 }, uvB: { u: 1, v: 1 }, uvC: { u: 1, v: 0 } },
+    { a: 4, b: 3, c: 5, uvA: { u: 0, v: 1 }, uvB: { u: 0, v: 0 }, uvC: { u: 1, v: 0 } },
+    { a: 4, b: 5, c: 6, uvA: { u: 0, v: 1 }, uvB: { u: 1, v: 0 }, uvC: { u: 1, v: 1 } },
 
     // back
-    { a: 6, b: 5, c: 7, uvA: { u: 0, v: 0 }, uvB: { u: 0, v: 1 }, uvC: { u: 1, v: 1 } },
-    { a: 6, b: 7, c: 8, uvA: { u: 0, v: 0 }, uvB: { u: 1, v: 1 }, uvC: { u: 1, v: 0 } },
+    { a: 6, b: 5, c: 7, uvA: { u: 0, v: 1 }, uvB: { u: 0, v: 0 }, uvC: { u: 1, v: 0 } },
+    { a: 6, b: 7, c: 8, uvA: { u: 0, v: 1 }, uvB: { u: 1, v: 0 }, uvC: { u: 1, v: 1 } },
 
     // left
-    { a: 8, b: 7, c: 2, uvA: { u: 0, v: 0 }, uvB: { u: 0, v: 1 }, uvC: { u: 1, v: 1 } },
-    { a: 8, b: 2, c: 1, uvA: { u: 0, v: 0 }, uvB: { u: 1, v: 1 }, uvC: { u: 1, v: 0 } },
+    { a: 8, b: 7, c: 2, uvA: { u: 0, v: 1 }, uvB: { u: 0, v: 0 }, uvC: { u: 1, v: 0 } },
+    { a: 8, b: 2, c: 1, uvA: { u: 0, v: 1 }, uvB: { u: 1, v: 0 }, uvC: { u: 1, v: 1 } },
 
     // top
-    { a: 2, b: 7, c: 5, uvA: { u: 0, v: 0 }, uvB: { u: 0, v: 1 }, uvC: { u: 1, v: 1 } },
-    { a: 2, b: 5, c: 3, uvA: { u: 0, v: 0 }, uvB: { u: 1, v: 1 }, uvC: { u: 1, v: 0 } },
+    { a: 2, b: 7, c: 5, uvA: { u: 0, v: 1 }, uvB: { u: 0, v: 0 }, uvC: { u: 1, v: 0 } },
+    { a: 2, b: 5, c: 3, uvA: { u: 0, v: 1 }, uvB: { u: 1, v: 0 }, uvC: { u: 1, v: 1 } },
 
     // bottom
-    { a: 6, b: 8, c: 1, uvA: { u: 0, v: 0 }, uvB: { u: 0, v: 1 }, uvC: { u: 1, v: 1 } },
-    { a: 6, b: 1, c: 4, uvA: { u: 0, v: 0 }, uvB: { u: 1, v: 1 }, uvC: { u: 1, v: 0 } },
+    { a: 6, b: 8, c: 1, uvA: { u: 0, v: 1 }, uvB: { u: 0, v: 0 }, uvC: { u: 1, v: 0 } },
+    { a: 6, b: 1, c: 4, uvA: { u: 0, v: 1 }, uvB: { u: 1, v: 0 }, uvC: { u: 1, v: 1 } },
 ];
 
 export const loadCubeMeshData = (): Mesh => {
@@ -73,10 +73,17 @@ export const loadObjFileData = async (filePath: string): Promise<Mesh> => {
 
     mesh.vertices = output.models[0].vertices.map((v) => new Vector3(v.x, v.y, v.z));
 
-    mesh.faces = output.models[0].faces.map((face) => ({ 
-            a: face.vertices[0].vertexIndex, 
-            b: face.vertices[1].vertexIndex, 
-            c: face.vertices[2].vertexIndex 
+    console.log(output.models[0]);
+
+    const texCoords = output.models[0].textureCoords;
+
+    mesh.faces = output.models[0].faces.map((face: ObjFileParser.Face) => ({ 
+            a: face.vertices[0].vertexIndex - 1, 
+            b: face.vertices[1].vertexIndex - 1, 
+            c: face.vertices[2].vertexIndex - 1,
+            uvA: texCoords[face.vertices[0].textureCoordsIndex - 1],
+            uvB: texCoords[face.vertices[1].textureCoordsIndex - 1],
+            uvC: texCoords[face.vertices[2].textureCoordsIndex - 1]
         })
     );
     //mesh.faces = face.fla
