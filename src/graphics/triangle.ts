26a5c784134d359301da76c56f6c8589a13da883
jinsubaek <bjinsu93@gmail.com>
2023-02-27 00:19:09 +0900

    feat: triangle 추가

--- src/graphics/triangle.ts
+++ src/graphics/triangle.ts
@@ -10,7 +10,33 @@ export interface Face {
     uvC: Texture
 }
  
-export interface Triangle {
-    points: Vector4[];
-    texCoords: Texture[];
+// export interface Triangle {
+//     points: Vector4[];
+//     texCoords: Texture[];
+// }
+
+export class Triangle {
+    private _points: Vector4[];
+    private _texCoords: Texture[];
+
+    public constructor(points: Vector4[], texCoords: Texture[] = []) {
+        this._points = points;
+        this._texCoords = texCoords;
+    }
+
+    get points(): Vector4[] {
+        return this._points;
+    }
+
+    get texCoords(): Texture[] {
+        return this._texCoords;
+    }
+
+    set points(value: Vector4[]) {
+        this._points = value;
+    }
+
+    set texCoords(value: Texture[]) {
+        this._texCoords = value;
+    }
 }
