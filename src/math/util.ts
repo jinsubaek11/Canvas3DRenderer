65b135a23d6f4965cf89c2b5d8e39e5d11613fd4
jinsubaek <bjinsu93@gmail.com>
2023-03-27 16:56:57 +0900

    feat: math util 함수 추가

--- src/math/util.ts
+++ src/math/util.ts
@@ -3,3 +3,13 @@ export const degreeToRadian = (deg: number): number => {
     
     return radian;
 }
+
+export const radianToDegree = (rad: number): number => {
+    const degree = rad / Math.PI * 180;
+
+    return degree;
+}
+
+export const lerp = (a: number, b: number, t: number): number => {
+    return (1 - t) * a + b * t;
+}
